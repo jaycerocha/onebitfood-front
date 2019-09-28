878a7fcc16d64b5fbc9643fb0d45f6bd141ca7a5
jaycerocha <jeyce_jp@hotmail.com>
2019-09-27 21:40:45 -0300

    enviando o pedido para API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React, {Fragment} from 'react';
-import "./App.scss";
-import { Container, Section } from 'rbx';
-import Header from "./components/header";
-import Routes from './routes';
-import ModalRoot from './components/modals';
+import axios from "axios";
 
-const App = () => (
-  <Fragment>
-    <Header />
-    <Section>
-      <Container>
-        <Routes/>
-      </Container>
-    </Section>
-    <ModalRoot />
-  </Fragment>
-)
+const url = axios.create({ baseURL: "http://localhost:3001" })
 
-export default App;
+export default {
+  loadRestaurants: (address, category) => {
+    let city_params = (address && address.city != undefined) ? `city=${address.city}` : ''
+    let category_params = (category != null) ? `&category=${category.title}` : ''
+
+    return url.get(`/restaurants?${city_params}${category_params}`)
+  },
+
+  searchRestaurants: (search) => url.get(`/restaurants/search?q=${search}`),
+  getRestaurant: (id) => url.get(`restaurants/${id}`),
+  loadCategories: () => url.get("/categories"),
+
+  createOrder: (order, products_order, address) => {
+    let new_product_orders = products_order.map(function (product_order) {
+      return ({
+        'product_id': product_order.product.id,
+        'comment': product_order.comment,
+        'quantity': product_order.quantity 
+      })
+    });
+    
+    let full_address = [  
+      address.street, address.number, address.city,
+      address.state, address.cep,
+      (address.reference)? `Referência: ${address.reference}` :  null,
+      (address.complement)? `Complemento: ${address.complement}` : null
+    ].join(',')
+    
+    order['order_products_attributes'] = new_product_orders
+    order['address'] = full_address
+
+    return url.post(`orders`, order)
+  },
+}
